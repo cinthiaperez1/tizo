a729954a8e8045c364812eabe23ee71fe610dc1b
MatiRosero <gekidasa@gmail.com>
2015-01-12 20:32:28 +0100

    logo topbar

--- includes/header.php
+++ includes/header.php
@@ -42,7 +42,7 @@
 			<nav class="top-bar" data-topbar role="navigation">
 				<ul class="title-area">
 					<li class="name">
-						<h1><a href="index.php"><img src="<?php echo $logo_url; ?>" alt="tizō" width="80" /></a></h1>
+						<h1><a href="index.php"><img src="<?php echo $logo_url; ?>" alt="tizō" /></a></h1>
 					</li>
 					<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
 					<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>

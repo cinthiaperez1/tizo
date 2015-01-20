c130e35be565c99a32c1c2a1e2c6ed1ffcb77fb5
cinthia1 <cinthiaestudio@gmail.com>
2015-01-20 20:18:38 +0100

    listo

--- user_form.php
+++ user_form.php
@@ -30,7 +30,8 @@ include('includes/header.php');
 					<!-- Cambiar esta parte siguiente por la que va en cada caso -->
 
 					<div class="small-12 columns">
-						<h2 class="form-name">Crear perfil usuario </h2>
+						<img class="user" src="#" alt="icono-usu"> 
+						<h2 class="form-name">Crear perfil de usuario </h2>
 					</div>
 
 					<!-- ----------------------------------------------------------------
@@ -66,133 +67,186 @@ include('includes/header.php');
 						<fieldset>
 							<legend>Información básica</legend>
 
-							<div class="row">
+							<!--cominza un campo de texto--><div class="row">
 								<div class="large-3 column">
-									<label>Nombre del proyecto</label>
+									<label>Nombre</label>
 								</div>
 								<div class="large-9 column">
-									<input type="text" name="create-project-name" id="create-project-name" placeholder="El nombre de tu proyecto" />
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
+								</div>
+							</div>
+							<!--termina-->
+							<div class="row">
+								<div class="large-3 column">
+									<label>Apellidos</label>
+								</div>
+								<div class="large-9 column">
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="large-3 column">
+									<label>Mail</label>
+								</div>
+								<div class="large-9 column">
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="large-3 column">
+									<label>Profesión</label>
+								</div>
+								<div class="large-9 column">
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
 
 							<div class="row">
 								<div class="large-3 column">
-									<label>Descripción (140 caracteres)</label>
+									<label>País</label>
 								</div>
 								<div class="large-9 column">
-									<textarea name="create-desc" id="create-desc" placeholder="Una breve descripción de tu proyecto"></textarea>
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
-
+							
 							<div class="row">
 								<div class="large-3 column">
-									<label>¿Mi proyecto está activo actualmente?</label>
+									<label>Ciudad</label>
 								</div>
 								<div class="large-9 column">
-									<input type="radio" name="is-active" value="Yes" id="active-yes" checked><label for="active-yes">Sí</label>
-									<input type="radio" name="is-active" value="No" id="active-no"><label for="active-no">No</label>
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
-
+							
 							<div class="row">
 								<div class="large-3 column">
-									<label>Imagen principal</label>
+									<label>Teléfono</label>
 								</div>
 								<div class="large-9 column">
-									SUBIR IMAGEN
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>							</d
+
+							
+								<div class="row">
+								<div class="large-3 column">
+									<label>Descripción</label>
+								</div>
+								<div class="large-9 column">
+									<textarea name="" id="" placeholder="Escribe algo breve sobre ti y tú profesión"></textarea>
 								</div>
 							</div>
+							
 
-						</fieldset>
+						</fields		
 
 						<fieldset>
-							<legend>Colaboración</legend>
+							<legend>Redes sociales de contacto</legend>
 
 							<div class="row">
 								<div class="large-3 column">
-									<label>Colaboradores</label>
+									
 								</div>
 								<div class="large-9 column">
-									<input type="text" name="create-contributer1" id="create-contributer1" placeholder="Escribe el nombre o correo de tu colaborador" />
-									<input type="text" name="create-contributer2" id="create-contributer2" placeholder="Escribe el nombre o correo de tu colaborador" />
-									<a class="more-contributors button tiny secondary" href="#">Agregar más</a>
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
 
-							<div class="row">
+							
+								<div class="row">
 								<div class="large-3 column">
-									<label>¿Quiero encontrar colaboradores?</label>
+									
 								</div>
 								<div class="large-9 column">
-									<input type="radio" name="find-contributors" value="Yes" id="find-yes" checked><label for="find-yes">Sí</label>
-									<input type="radio" name="find-contributors" value="No" id="find-no"><label for="find-no">No</label>
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
 
-							<div class="row">
+							
+								<div class="row">
 								<div class="large-3 column">
-									<label>Tipo de colaboración que busco</label>
+									
 								</div>
 								<div class="large-9 column">
-									<input name="contribute-type" id="contribute-time" type="checkbox"><label for="contribute-time">Tiempo (quiero ser voluntario)</label><br />
-									<input name="contribute-type" id="contribute-materials" type="checkbox"><label for="contribute-materials">Materiales</label><br />
-									<input name="contribute-type" id="contribute-knowledge" type="checkbox"><label for="contribute-knowledge">Conocimiento</label><br />
-									<input name="contribute-type" id="contribute-funds" type="checkbox"><label for="contribute-funds">Fondos</label><br />
-									<input name="contribute-type" id="contribute-other" type="checkbox"><label for="contribute-other">Otro (describir abajo)</label><br />
-									<input type="text" name="contribute-other-type" id="contribute-other-type" placeholder="Cómo puedo colaborar con el proyecto" />
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
+								</div>
+							</div>
 
-
-
+							
+								<div class="row">
+								<div class="large-3 column">
+									
+								</div>
+								<div class="large-9 column">
+									<input type="text" name="create-project-name" id="create-project-name" placeholder=""/>
 								</div>
 							</div>
 
 						</fieldset>
 
-						<fieldset>
-							<legend>Más detalles</legend>
-
-							<div class="row">
-								<div class="large-3 column">
-									<label>Pregunta 1</label>
-								</div>
-								<div class="large-9 column">
-									<textarea name="" id="" placeholder=""></textarea>
-								</div>
-							</div>
-
-							<div class="row">
-								<div class="large-12 column">
-									<label>Pregunta 1</label>
-
-									<textarea name="" id="" placeholder=""></textarea>
-								</div>
-							</div>
-
-						</fieldset>
-
-						<fieldset>
-							<legend>Galería</legend>
-
-							<div class="row">
-								<div class="large-12 column">
-									Subir archivos
-								</div>
-							</div>
-
-						</fieldset>
-
+						
 						<div class="row">
 							<div class="small-12 column">
-								<button type=submit name="create-submit" id="create-submit">Crear proyecto</button>
-								<button class="secondary" type=submit name="create-draft-submit" id="create-draft-submit">Guardar borrador</button>
+								<button type=submit name="create-submit" id="create-submit">Guardar</button>
 							</div>
 						</div>
-
+						
 					</form>
 
+					
+					
+					
+					<div class="row">
+								<div class="large-12 column">
+									<section class="tags">
+									
+									<h2>Tags de búsqueda</h2>
+									<p>Escoge tus temas de preferencia para personalizar tu área de exploración <br>
+										y encontrar los proyectos relacionados que más te interesan</p>
+										
+										<div class="row">
 
+											<div class="large-12 column">
+												
+												<div id="tags">
+													 
+													<button  type="submit" onclick=""> Arte
+														<img class="mail-ico" src="img/mail.svg" alt="icono mail"> </button>
+								
+													 <button  type="submit" onclick=""> Ecología y medio ambiente
+														 <img class="mail-ico" src="img/mail.svg" alt="icono mail"> </button>
+													
+														  <button  type="submit" onclick=""> Multicultura
+														<img class="mail-ico" src="img/mail.svg" alt="icono mail"> 
+												
+												    </button>
+												</div>	
+										
+											</div>
+										
+										
+														 </div>	
+									
+									</section>
 				</div>
+			
+										
+										
 			</div>
+					
+					
+					
+					
+					
+					
+					
+					
+					
+
+					
+					
+					
 			<!-- END fom-main -->
 
 
@@ -208,8 +262,3 @@ include('includes/header.php');
 		<!-- END main-content -->
 
 
-<?php
-
-include('includes/footer.php');
-
-?>
